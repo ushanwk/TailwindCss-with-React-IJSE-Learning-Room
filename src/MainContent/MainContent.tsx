faa12c86d57de90eb1b9454798e737df263f753d
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-10 11:38:40 +0530

    header gradient

--- src/MainContent/MainContent.tsx
+++ src/MainContent/MainContent.tsx
@@ -4,7 +4,7 @@ export class MainContent extends Component {
     render() {
         return (
             <>
-                <div className="bg-blue-50 pt-64 pb-64">
+                <div className="bg-blue-50 pt-80 pb-80">
                     <h1 className="text-center text-blue-400 text-3xl font-bold">This is Main Content</h1>
                 </div>
             </>
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ export class Navbar extends Component {
     render() {
         return (
             <>
-                <div className="bg-blue-400 p-3">
+                <div className="bg-blue-400 bg-gradient-to-r from-blue-600 p-3">
                     <h1 className="text-center text-white font-bold">This is NavBar</h1>
                 </div>
             </>
