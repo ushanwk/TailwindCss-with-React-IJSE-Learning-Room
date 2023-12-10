b8a5edf9ef633c2765a1ecafc2eb9e3b01e84946
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-10 12:09:09 +0530

    box

--- src/MainContent/MainContent.tsx
+++ src/MainContent/MainContent.tsx
@@ -4,8 +4,14 @@ export class MainContent extends Component {
     render() {
         return (
             <>
-                <div className="bg-blue-50 pt-80 pb-80">
+                <div className="bg-blue-200 bg-gradient-to-r from-blue-50 pt-72 pb-72">
+
                     <h1 className="text-center text-blue-400 text-3xl font-bold">This is Main Content</h1>
+
+                    <div className="mt-10 w-20 h-20 bg-blue-400 shadow-blue-200 shadow-xl hover:shadow-blue-300 mx-auto">
+                        <h1 className="text-white font-bold pt-7 pl-5">Hello</h1>
+                    </div>
+
                 </div>
             </>
         );
