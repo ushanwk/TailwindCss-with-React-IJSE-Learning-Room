cccd41ae6cf8dcb71bb67dc963d19bfa0108e92d
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-10 11:39:49 +0530

    footer gradient

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -4,7 +4,7 @@ export class Footer extends Component {
     render() {
         return (
             <>
-                <div className="bg-gray-400 p-3">
+                <div className="bg-gray-400 bg-gradient-to-r from-gray-600 p-3">
                     <h1 className="text-center text-white font-bold">This is Footer</h1>
                 </div>
             </>
