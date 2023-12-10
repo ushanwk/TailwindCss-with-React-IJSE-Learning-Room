c6d2a2457cc62e6b3a4da06dae673646e123f054
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-10 14:34:54 +0530

    button nav

--- src/view/Navbar/Navbar.tsx
+++ src/view/Navbar/Navbar.tsx
@@ -8,6 +8,7 @@ export class Navbar extends Component {
                 <div className="bg-blue-400 bg-gradient-to-r from-blue-600 p-3 flex">
                     <img src={logo} className="w-6 h-6 mt-2"/>
                     <h1 className="mx-auto text-white font-bold m-2">This is NavBar</h1>
+                    <button className="bg-blue-50 pl-2 pr-2 border-2 border-white rounded-[8px] text-blue-400">Sign In</button>
                 </div>
             </>
         );
--- src/view/footer/Footer.tsx
+++ src/view/footer/Footer.tsx
@@ -1,11 +1,13 @@
 import {Component} from "react";
+import copyright from "../../image/copyright.png";
 
 export class Footer extends Component {
     render() {
         return (
             <>
-                <div className="bg-gray-400 bg-gradient-to-r from-gray-600 p-3">
+                <div className="bg-gray-400 bg-gradient-to-r from-gray-600 p-3 flex mx-auto justify-center">
                     <h1 className="text-center text-white font-bold">This is Footer</h1>
+                    <img src={copyright} className="w-5 h-5 ml-3 mt-0.5"/>
                 </div>
             </>
         );
